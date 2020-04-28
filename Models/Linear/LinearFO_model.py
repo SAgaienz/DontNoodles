3a99519117ae54799689c7e4e6d5f0de2c91711b
SAgaienz <u16021518@tuks.co.za>
2020-04-28 16:12:39 +0200

    Finished Linear (FO) Model and added "README"

--- Models/Linear/LinearFO_model.py
+++ Models/Linear/LinearFO_model.py
@@ -1,28 +1,37 @@
 import numpy as np 
 import pandas 
 import matplotlib.pyplot as plt 
-from scipy.optimize import curve_fit
+from scipy.optimize import curve_fit, fsolve
 from scipy.signal import lti, lsim
+from scipy.interpolate import interp1d
 
+# 70L step test data
 df = pandas.read_csv('Data/70L_Step_data_Cleaned.csv')
 
 xdata, ydata = df['time'].tolist(), df['tem'].tolist()
 xdata1, ydata1 = xdata[0:1400], ydata[0:1400]
 
 
-st = 33.358
+# initial/on values 
+i_on = 7
+t_on = xdata[i_on]
 T0 = np.mean(ydata[0:5])
-tspan = np.linspace(0, 9000, 2000)
 
-t_to_boil = 4.186*70*(ydata[-1] - ydata[0])/3
-print(t_to_boil)
+# The Values where T_final = 92C
+i_f = 1397 #index
+t_f = xdata[i_f]
+T_f = ydata[i_f]
 
+tspan = np.linspace(0, 11000, 20000)
+
+# Time that we turned the element on at (unit step)
 def U(t):
-    if t<st:
+    if t<t_on:
         return 0
-    if t>=st:
+    if t>=t_on:
         return 3000
-
+        
+# First order Model
 def fo(tspan, K, τ ):
     Uspan = [U(t) for t in tspan]
     Gp = lti(K, [τ , 1 ])
@@ -30,9 +39,40 @@ def fo(tspan, K, τ ):
     Tl = Tl + T0
     return Tl
 
-B, res =  curve_fit(fo, xdata1, ydata1, [100000, 5000])
-plt.plot(tspan, fo(tspan, *B), label = 'FO model' , color = '#c2510c')
-plt.plot(xdata, ydata, label = 'Exp Data from 70L Step Test', color = '#10bbc7')
-plt.text(1500,30, 'K = '+ str(B[0]) + ',  τ = '+ str(B[1])  )
+# Regression
+B70, res =  curve_fit(fo, xdata1, ydata1, [100000, 5000])
+
+# Because of the COVID-19 Pandemic, it is impossible to find the actual dependence of the system parameters (physical properties) so we will infer them as follows...
+# Assume that the factor by which the time to boil is extended by over ideality is constant for all volumes...
+# We will call this factor κ = (Time it actually took to get to 95C) / (time it theoretically should take to get to 95C) (s/s)
+t_t_b_70 = 4.186*70*(T_f - T0)/3 #Theoritcal time taken to get to 92C from 27C for 70L (Cp calc: P = 3kW, Cp = 4.186)
+κ = ((t_f - t_on)/t_t_b_70)
+def time_to_boil(V, κ):
+    return (4.186*V*(T_f - T0)/3)*κ 
+
+# We will assume that the gain of the system has no dependance on volume (the gain of the FO linear model is constant)
+def K_and_τ( tf, Tf): 
+    K = B70[0] # constant K (from bit)
+    τ = -tf/(np.log(1 - ((Tf - T0)/(K*3000))))# Time domain response of FO, solved for τ 
+    return [K,τ]
+
+Vspan = np.arange(30, 100, 10)
+parms = {}
+for V in Vspan:
+    t_end = time_to_boil(V, κ)
+    K, τ  = K_and_τ(t_end, 92)
+    parms['V' + str(V)] = K, τ
+# This (above) becomes unnecessary if we just sub "time_to_boil" into "K_and_τ" and sub 1 for "V" in "time_to_boil"
+# # Bringing the dependence together...
+def Params(V):
+    K, τ = 0.06519652149867068 , 265.02732767188155*V
+    return K, τ 
+
+for V in [26.8, 80.51, 62, 70]:
+    plt.plot(tspan, fo(tspan, *Params(V)), label = 'Volume = ' + str(V)+' L')
+plt.plot(xdata, ydata, label = 'Volume = 70 L (Experimental Data)')
 plt.legend(loc = 'best')
+plt.ylabel('Temperature, °C')
+plt.xlabel('Time, s')
+plt.axis([-15, 11000,25, 100 ])
 plt.show()
---
+++ README.md
@@ -0,0 +1,6 @@
+## A Tuks Brewery Project - Dynamics and Control 
+
+A repository for a research project from the department of Chemical Engineering at the University of Pretoria (Tuks). This undergraduate research project is aimed to explore the nature of the linearity and general dynamics (modelling) of a Brewery's (Tuks Brewery) vessel. The project also aims to offer control solutions for this brewing vessel (hot liqour tank).
+
+Models (non-linear and linear) models, simulation files, Data (temperature logs) and figures, used for this research project, are all found in this repository. 
+
