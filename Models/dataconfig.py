4735877cf5fc97e176802320aa42ba32fbdbd423
SAgaienz <u16021518@tuks.co.za>
2020-06-14 22:58:35 +0200

    modified:   Models/Model_Linear_SO_Characterisation.ipynb 	modified:   Models/__pycache__/dataconfig.cpython-38.pyc 	modified:   Models/config.ini 	modified:   Models/dataconfig.py

--- Models/dataconfig.py
+++ Models/dataconfig.py
@@ -1,6 +1,18 @@
-import configparser, pathlib
+import configparser, pathlib, os
+from sys import platform
+
 
 config = configparser.ConfigParser()
-config.read('config.ini')
-datadir = pathlib.Path(config['Paths']['bulkdir']).expanduser()
+if os.path.exists(r'Models/config.ini'):
+    config.read(r'Models/config.ini')   
 
+    if platform == "linux":
+        datadir = pathlib.Path(config['PathLinux']['bulkdir']).expanduser()
+    if platform == "win32":
+        datadir = pathlib.Path(config['PathWin']['bulkdir']).expanduser()
+    if platform == "darwin":
+        datadir = pathlib.Path(config['PathMac']['bulkdir']).expanduser()
+    print('Current Data dir is "' + str(datadir)+'"')
+    print('OS: ' + str(platform))
+else:
+    print('File "config.ini" cannot be found')
